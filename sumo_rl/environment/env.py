83f8ffe14d1878a02c580be4858b6eab8f0a18f2
Keny Patel <68371195+patelkeny92@users.noreply.github.com>
2025-08-23 00:18:26 +0100

    Update env.py
    I have made changes to import AgentSelector for newer pettingzoo versions.

--- sumo_rl/environment/env.py
+++ sumo_rl/environment/env.py
@@ -21,13 +21,13 @@ from pettingzoo import AECEnv
 from pettingzoo.utils import wrappers
 
 
-try:
-    # pettingzoo 1.25+
-    from pettingzoo.utils import AgentSelector
-except ImportError:
-    # pettingzoo 1.24 or earlier
-    from pettingzoo.utils import agent_selector as AgentSelector
-
+# try:
+#     # pettingzoo 1.25+
+#     from pettingzoo.utils import AgentSelector
+# except ImportError:
+#     # pettingzoo 1.24 or earlier
+#     from pettingzoo.utils import agent_selector as AgentSelector
+from pettingzoo.utils.agent_selector import AgentSelector
 from pettingzoo.utils.conversions import parallel_wrapper_fn
 
 from .observations import DefaultObservationFunction, ObservationFunction
